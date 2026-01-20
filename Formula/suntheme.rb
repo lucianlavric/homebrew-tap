536819a1de2496b41dadb0d619877326bc2ef2ff
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2026-01-20 00:58:50 -0500

    Update suntheme to v0.1.1

--- Formula/suntheme.rb
+++ Formula/suntheme.rb
@@ -1,22 +1,22 @@
 class Suntheme < Formula
   desc "Automatic theme switching for Ghostty and Neovim based on sunrise/sunset"
   homepage "https://github.com/lucianlavric/suntheme"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/lucianlavric/suntheme/releases/download/v#{version}/suntheme-aarch64-apple-darwin.tar.gz"
-      sha256 "db7ad6b5d2d81dae25da282763b5b59657b865fb8e2c5262ec522c01057c418b"
+      sha256 "b7c17b92591f704a8a151b3b70e72cbe7a7bd1e338e3d2052be010b5c54f3ed8"
     else
       url "https://github.com/lucianlavric/suntheme/releases/download/v#{version}/suntheme-x86_64-apple-darwin.tar.gz"
-      sha256 "723b419f60b97a2ade1c9f45ef6eae7a2f0279abe9539e147c49c5cc2c036360"
+      sha256 "76d70f0a35c5ccd09be79897fd165822db9079901276ee8b80081d2e84d65868"
     end
   end
 
   on_linux do
     url "https://github.com/lucianlavric/suntheme/releases/download/v#{version}/suntheme-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "692115bb5026323b06d2ea6849a5a23d62454bf814a201cf3777ea007c93ab63"
+    sha256 "a637c71553d4c9b7f9e6ac41bc0c127fbf7aa829d16aaef4371bf34cb5700dbd"
   end
 
   def install
